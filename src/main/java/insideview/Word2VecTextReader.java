fbcda0581536c0a2485172056b791b753ff6599a
nyghtowl <warrick.melanie@gmail.com>
2015-05-18 13:13:51 -0700

    Created initial example to read text file, apply word2vec with spark and output text file of word vectors.

---
+++ src/main/java/insideview/Word2VecTextReader.java
@@ -0,0 +1,84 @@
+package insideview;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.wordstore.inmemory.InMemoryLookupCache;
+import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable;
+import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
+import org.deeplearning4j.spark.models.embeddings.word2vec.Word2Vec;
+import org.deeplearning4j.spark.models.embeddings.word2vec.Word2VecPerformer;
+import org.deeplearning4j.berkeley.Pair;
+import org.deeplearning4j.models.embeddings.WeightLookupTable;
+import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
+
+import org.deeplearning4j.util.SerializationUtils;
+
+import java.io.File;
+
+/**
+ * Reading in text file
+ * Converting to word vectors
+ * Saving vectors as text
+ */
+
+public class Word2VecTextReader
+{
+    private static JavaSparkContext connectSpark(int minWords, int vectorLength){
+
+        SparkConf sparkConf = new SparkConf()
+                .setMaster("local[8]").set(SparkDl4jMultiLayer.AVERAGE_EACH_ITERATION, "false").set(Word2VecPerformer.NUM_WORDS, String.valueOf(minWords)).set(Word2VecPerformer.NEGATIVE, String.valueOf("0"))
+                .set("spark.akka.frameSize", "100").set(Word2VecPerformer.VECTOR_LENGTH,String.valueOf(vectorLength))
+                .setAppName("mnist");
+
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        return sc;
+    }
+
+    private static Pair<VocabCache,WeightLookupTable> vectorizeWords(String fileName, JavaSparkContext sc) throws Exception {
+        JavaRDD<String> rdd = sc.textFile(new File(fileName).toURI().toString());
+        Word2Vec vec = new Word2Vec();
+        return vec.train(rdd);
+    }
+
+    private static void saveVectors( Pair result, String outputFileName, Boolean serialize ) throws Exception {
+        if (serialize) {
+            SerializationUtils.saveObject(result, new File(outputFileName));
+            return;
+        }
+        InMemoryLookupCache vecCache = (InMemoryLookupCache) result.getFirst();
+        InMemoryLookupTable vecTable = (InMemoryLookupTable) result.getSecond();
+        WordVectorSerializer.writeWordVectors(vecTable, vecCache, outputFileName);
+
+    }
+
+    public static void main( String[] args ) throws Exception
+    {
+        // Get filename
+        String inputFilePath = "";
+        String outputFileName = "";
+        Boolean serialize = false;
+        int minWords = 1;
+        int vectorLength = 300;
+
+        if(args.length >= 2) {
+            inputFilePath = args[0];
+            outputFileName = args[1];
+            serialize = Boolean.valueOf(args[2]);
+        } else {
+            System.err.println("Please enter directory plus filename for the text file you want to convert, and enter the file name for vector output.");
+        }
+
+        System.out.println("Setting up Spark Context...");
+        JavaSparkContext sc = connectSpark(minWords, vectorLength);
+
+        System.out.println("Vectorizing words...");
+        Pair<VocabCache,WeightLookupTable> result = vectorizeWords(inputFilePath, sc);
+
+        saveVectors(result, outputFileName, serialize);
+        System.out.println("Model saved");
+
+
+    }
+}
