4724367420770a745f4149718be68d7ac856d11d
sonaliii <sona8242@gmail.com>
2015-05-18 15:56:33 -0700

    Updates junit version to allow for @Test annotation

--- src/test/java/insideview/Word2VecTextReaderTest.java
+++ src/test/java/insideview/Word2VecTextReaderTest.java
@@ -1,38 +1,14 @@
 package insideview;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit tests.
  */
-public class Word2VecTextReaderTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public Word2VecTextReaderTest(String testName)
-    {
-        super( testName );
+public class Word2VecTextReaderTest {
+    @Test
+    public void testWord2VecTextReader() {
+        
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( Word2VecTextReader.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
